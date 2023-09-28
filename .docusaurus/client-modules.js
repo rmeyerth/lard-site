38dd759c77df9e5bcce6256f5c6473b4b6cb4c9f
Robert Meyer <tronied@yahoo.com>
2023-09-28 21:05:06 +0100

    Removed invisible .docusaurus hidden files

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('C:\\github\\lard-site\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('C:\\github\\lard-site\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('C:\\github\\lard-site\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('C:\\github\\lard-site\\src\\css\\custom.css'),
-];
