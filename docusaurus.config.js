465fcbebe136411cb7fae3cfa57355cc73458194
Robert Meyer <robert.meyer@ardonaghspecialty.com>
2024-12-12 20:46:00 +0000

    Removed another comment to trigger build

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,6 @@ const config = {
   tagline: 'The language chef’s secret ingredient!',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
   url: 'https://your-docusaurus-test-site.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
